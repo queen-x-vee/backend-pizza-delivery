41f4e5e8c325e0a2782693cc90403801030f19d8
valentina buoro <valentinabuoro4@gmail.com>
2023-11-20 23:10:56 +0100

    second commit

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-engine = create_engine('postgresql://valentinabuoro@localhost://5432/pizza-delivery'
-                       , echo=True)
+engine = create_engine('postgresql://valentinabuoro@localhost/pizza-delivery', echo=True)
 
 Base = declarative_base()
 
--- init_db.py
+++ init_db.py
@@ -1,5 +1,5 @@
 from database import engine, Base
-from models import User, Order
+from models import User, Choice
 
 
 Base.metadata.create_all(bind=engine)
